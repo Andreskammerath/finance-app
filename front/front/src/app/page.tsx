b3d60de7ad6226b1aa2add328785cb15495f7e19
Matias Vazquez <matias.vazquez@minimalart.co>
2024-04-25 21:03:09 -0300

    Initial commit. Project set up.

---
+++ front/front/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+// import styles from "./page.module.css";
+import Button from "./button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello world</h1>
+      <Button />
+    </main>
+  );
+}
